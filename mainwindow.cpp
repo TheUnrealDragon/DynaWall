bf4d8b7329dea6edcf939846b65a74a4b8903f80
UnrealDragon <alfmatabr03@gmail.com>
2022-11-04 16:01:00 +0530

    inital commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,201 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    std::ifstream f(config);
+    if(f.is_open())
+    {
+        std::cout << "file read start" << std::endl;
+        std::string str;
+        std::stringstream buffer;
+        buffer << f.rdbuf();
+        str = buffer.str();
+        std::cout << "file read complete" << std::endl;
+        QJsonDocument doc = QJsonDocument::fromJson(QByteArray::fromStdString(str));
+        std::cout << "==============" << std::endl;
+        std::cout << str << std::endl;
+        std::cout << "==============" << std::endl;
+        topsettings = doc.object();
+        QJsonObject settings = topsettings["ApplicationSettings"].toObject();
+
+        //Config 1: Folder Path
+        if(settings["wallpaperpath"].isNull() || settings["wallpaperpath"].isUndefined())
+        {
+            std::cout << "[WARN] No Wallpaper Directory Selected (Ignore if running for first time)" << std::endl;
+        }
+        else
+        {
+            std::cout << "[INFO] Directory: " << settings["wallpaperpath"].toString().toStdString() << std::endl;
+            ui->wallpaperFolder->setText(settings["wallpaperpath"].toString());
+            on_pushButton_clicked();
+        }
+    }
+    else
+    {
+        std::cout << "Creating Config File in Application Working Directory" << std::endl;
+        QJsonObject settings;
+        QJsonValue val = settings;
+        QJsonObject obj{
+            {"ApplicationSettings",val}
+        };
+        QJsonDocument doc(obj);
+        QByteArray arr =  doc.toJson();
+        f.close();
+        std::fstream d;
+        d.open(config,std::fstream::out);
+        if(!d.is_open())
+        {
+            std::cerr << "Cannot write to config file!" << std::endl;
+            return;
+        }
+        else
+        {
+            d << arr.toStdString();
+            d.flush();
+        }
+        d.close();
+    }
+    if(f.is_open())
+        f.close();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    if(ui->wallpaperFolder->text().isEmpty())
+        return;
+    std::fstream f(config);
+    f.open(config,std::fstream::in);
+    if(f.is_open())
+    {
+        //Check if the folder is the same:
+        //No need to read from f as topsettings should have the read values from MainWindow Constructor
+        QJsonObject settings = topsettings["ApplicationSettings"].toObject();
+        if(settings.isEmpty())
+        {
+            std::cerr << "[WARN] Config file is corrupt: Settings empty " << std::endl;
+            settings.insert("wallpaperpath",QJsonValue(ui->wallpaperFolder->text()));
+            topsettings["ApplicationSettings"] = settings;
+            //Write Settings and call function again!
+            QJsonDocument doc(topsettings);
+            f.close();
+            std::fstream d;
+            d.open(config,std::fstream::out);
+            if(!d.is_open())
+            {
+                std::cerr << "[ERR] Cannot open Config file to Write" << std::endl;
+                return;
+            }
+            d << doc.toJson().toStdString();
+            d.flush();
+            d.close();
+            on_pushButton_clicked();
+            return;
+        }
+        QJsonValue path = settings["wallpaperpath"];
+        if(path.isUndefined())
+        {
+            std::cerr << "[ERR] Config file is corrupt: Path Undefined" << std::endl;
+            return;
+        }
+        else if(path.toString() == ui->wallpaperFolder->text())
+        {
+            //PATH IS SAME NOTHING TO DO
+        }
+        else
+        {
+            //Update path
+            path = ui->wallpaperFolder->text();
+            settings["wallpaperpath"] = path;
+            //Write settings;
+            topsettings["ApplicationSettings"] = settings;
+            f.close();
+            QJsonDocument writer;
+            writer.setObject(topsettings);
+            std::string d = writer.toJson().toStdString();
+            std::fstream w;
+            w.open(config,std::fstream::out);
+            if(!w.is_open())
+            {
+                std::cerr << "[WARN] Can not write to Config file" << std::endl;
+            }
+            w << d;
+            w.flush();
+            w.close();
+        }
+
+    }
+    else
+    {
+        std::cout << "Creating Config File in Application Working Directory" << std::endl;
+        QJsonObject settings;
+        QJsonValue v = ui->wallpaperFolder->text();
+        settings.insert("wallpaperpath",v);
+        QJsonObject obj{
+            {"ApplicationSettings",settings}
+        };
+        QJsonDocument doc(obj);
+        QByteArray arr =  doc.toJson();
+        f.close();
+        std::fstream d;
+        d.open(config,std::fstream::out);
+        if(!d.is_open())
+        {
+            std::cerr << "Cannot write to config file!" << std::endl;
+            return;
+        }
+        else
+        {
+            d << arr.toStdString();
+            d.flush();
+        }
+        d.close();
+    }
+    if(f.is_open())
+        f.close();
+
+    int i = 0;
+    int j = 0;
+    for(auto const &dir_entry : std::filesystem::recursive_directory_iterator{ui->wallpaperFolder->text().toStdString()})
+    {
+        std::cout << dir_entry << std::endl;
+        QLabel *label = new QLabel(this);
+        std::stringstream is;
+        is << dir_entry;
+        std::string p;
+        is >> p;
+        label->resize(300,300);
+        std::cout << "p is: " << p << std::endl;
+        p.erase(0,1);
+        p.erase(p.size()-1);
+        QPixmap pixmap(QString::fromStdString(p));
+        if(pixmap.isNull())
+        {
+            std::filesystem::path path = dir_entry.path();
+            if(path.extension() == ".heic")
+            {
+                std::cout << "[INFO] heic image found!" << std::endl;
+                using namespace std::chrono_literals;
+                std::this_thread::sleep_for(2000ms);
+            }
+            std::cout << path << " is not an image!" << std::endl;
+            continue;
+        }
+        label->setPixmap(pixmap.scaled(label->size(),Qt::KeepAspectRatio));
+        label->show();
+        ui->gridLayout_3->addWidget(label,i,j++);
+        if((j+1)%4 == 0)
+        {
+            i++;
+            j=0;
+        }
+    }
+
+
+
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,19 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <filesystem>
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <vector>
+#include <QLabel>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QJsonValue>
+#include <QPixmap>
+#include <sstream>
+#include <fstream>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -10,10 +23,15 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
+    std::vector<QLabel *> labellist;
+    std::string config="config.json";
+    QJsonObject topsettings;
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
